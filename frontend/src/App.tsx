87cc780ca7f7f08e0c49fa7f05e8917631cb296f
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-07-03 17:18:54 +0100

    setting up backend with nest/prisma

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Login from "./pages/LogIn";
+import React, { useState } from 'react'
+import './App.css'
+import Login from './pages/LogIn'
 
 function App() {
-  const [Logged, setLogged] = useState(false);
+  const [Logged, setLogged] = useState(false)
   return (
     <div className="App">
       <Login />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
